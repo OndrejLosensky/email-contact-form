f146c34401d3c72e6164df92b3508c48be5d2df8
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-24 21:46:29 +0200

    stylování formuláře který logguje data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import ContactForm from "./components/Contact Form/ContactForm";
 import Footer from "./components/footer/Footer";
 
 import ContactTest from "./components/Contact Form/ContactTest";
@@ -7,7 +6,6 @@ import ContactTest from "./components/Contact Form/ContactTest";
 function App() {
   return (
     <section className="w-sceen h-full bg-bodyColor">
-       {/*<ContactForm/>*/} 
         <ContactTest/>
         <Footer/>
     </section>
