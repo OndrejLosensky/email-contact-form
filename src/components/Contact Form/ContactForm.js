1b2e92893ad1671da9ea54eb059b3b788464f226
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-24 20:19:42 +0200

    zahajeni

---
+++ src/components/Contact Form/ContactForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactForm = () => {
+    return (
+        <div>
+            <p className="text-3xl"> test</p>
+        </div>
+    )
+}
+
+export default ContactForm
