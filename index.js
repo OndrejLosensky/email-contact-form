bd65a9b3d20d8ad6432525e14587f73f45db35c6
Ondrej Losensky <ondra.losi@gmail.com>
2023-09-25 18:59:44 +0200

    server pro odesilani emailu

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors()); 
+
+
+app.get('/', () => {
+    resizeBy.send("welcome to my forma")
+})
+
+app.post('/api/forma', (req, res) => {
+
+    let data = req.body
+    let smtpTransport = nodemailer.createTransport({
+        service: 'Gmail', 
+        port: 465,
+        auth: {
+            user:'ondra.losi@gmail.com',
+            pass:'Losicek0505'
+        }
+    });
+
+    let mailOptions = {
+        from:data.email,
+        to: 'ondra.losi@gmail.com',
+        subject:`Zprava od ${data.name}`,
+        html:`
+            <h2> Informace </h2>
+
+            <li> Jmeno: ${data.name}</li>
+            <li> Email: ${data.email} </li>
+            <li> Zprava: ${data.message} </li>
+
+
+        `
+    };
+
+    smtpTransport.sendMai(mailOptions, (error, response) => {
+        if(error)
+        {
+            res.send(error)
+        }
+        else
+        {
+            res.send('Success')
+        }
+    })
+})
